bf94cd97e4665497b12620a89d4bc7303edf7ffd
HamidRahmanii <hamidrahmani81@yahoo.com>
2025-02-24 21:06:46 +0330

    first  commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import react from "react";
+//Components and Pages
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Ignite</h1>
+    </div>
+  );
+}
+
+export default App;
