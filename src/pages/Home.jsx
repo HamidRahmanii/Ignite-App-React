50ae6c1df8d471f25153940efc8d2bbb9d5e6984
HamidRahmanii <hamidrahmani81@yahoo.com>
2025-02-25 18:32:11 +0330

    add Game component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,45 @@
 import react, { useEffect } from "react";
 
 //REDUX
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gamesAction";
+
+//Components
+import Game from "../components/Game";
+
+//Styling and Animation
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const Home = () => {
   //FETCH GAMES
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(loadGames());
-  });
+  }, [dispatch]);
+
+  //Get that data back
+  const { popular, upcoming, newGames } = useSelector((state) => state.games);
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <GameList>
+      <h2>Upcoming Games</h2>
+      <Games>
+        {upcoming.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            image={game.background_image}
+            key={game.id}
+          />
+        ))}
+      </Games>
+    </GameList>
   );
 };
+
+const GameList = styled(motion.div)``;
+const Games = styled(motion.div)``;
 
 export default Home;
